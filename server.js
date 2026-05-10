4e716812926a84af801464964ad4d512d334d192
AbuBakkarSabitth <140878004+AbuBakkarSabitth@users.noreply.github.com>
2026-05-10 10:39:29 +0600

    Updated PORT for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", (req,res) => {
     res.send("AI Resume Analyzer API is running...");
 });
 app.use("/",analyzeRoute);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
